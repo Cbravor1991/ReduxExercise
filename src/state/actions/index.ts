19f4d7e9cf6f80414700fc0961d6474ae399f2d9
RVLareu <rlareu@fi.uba.ar>
2022-05-26 16:40:10 -0300

    exported actions created

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,4 +1,26 @@
 import { changeSample } from './changeSample';
 import { updateOtherSample } from './updateOtherSample';
+import { addition } from './addition';
+import { newValue } from './newValue';
+import { substraction } from './substraction';
+import { multiplication } from './multiplication';
+import { divition } from './divition';
+import { squareRoot } from './squareRoot';
+import { summation } from './summation';
+import { intro } from './intro';
+import { undo } from './undo';
+import { dot } from './dot';
 
-export { changeSample, updateOtherSample };
+export { changeSample,
+        updateOtherSample,
+        addition,
+        newValue,
+        substraction,
+        multiplication,
+        divition,
+        squareRoot,
+        summation,
+        intro,
+        undo,
+        dot
+    };
