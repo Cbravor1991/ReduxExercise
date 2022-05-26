ffcd4cf31669ba64fe015320e504ba7964267b2b
RVLareu <rlareu@fi.uba.ar>
2022-05-26 16:38:17 -0300

    added polishCalcReducer to rootReducer

--- src/state/reducer/index.ts
+++ src/state/reducer/index.ts
@@ -3,10 +3,13 @@ import { combineReducers } from 'redux';
 import { sampleReducer } from './sampleReducer';
 import { otherSampleReducer } from './otherSampleReducer';
 import { higherOrderReducer } from './higherOrderReducer';
+import { polishCalcReducer } from './polishCalcReducer';
+
 
 export const rootReducer = higherOrderReducer(
   combineReducers({
     sample: sampleReducer,
     otherSample: otherSampleReducer,
+    polish: polishCalcReducer
   })
 );
