ded4d648035f87045d1429d3cce114c2804b0030
RVLareu <rlareu@fi.uba.ar>
2022-05-26 16:40:46 -0300

    added onClick function to manage different operations

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,9 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import { changeSample } from 'state/actions/changeSample';
-import { updateOtherSample } from 'state/actions/updateOtherSample';
+
 import { selectCurrentNumber } from 'state/selectors/selectCurrentNumber';
 import { selectCurrentStack } from 'state/selectors/selectCurrentStack';
+import { addition,
+          newValue,
+          substraction,
+          multiplication,
+          divition,
+          squareRoot,
+          summation,
+          intro,
+          undo,
+          dot } from 'state/actions';
+
 
 import styles from './Calculator.module.css';
 
@@ -16,14 +26,52 @@ export const Calculator = () => {
   const stack = useSelector(selectCurrentStack);
 
   const dispatch = useDispatch();
+  
   const onClickNumber = (n: number) => {
-    const action = changeSample();
-    dispatch(action);
+    const newValueAction = newValue(n);
+    dispatch(newValueAction);
   };
-  const onClick = () => {
-    const action = updateOtherSample();
-    dispatch(action);
+  const onDotClick = () => {
+    const dotAction = dot();
+    dispatch(dotAction);
   };
+
+  const onAddClick = () => {
+    const addAction = addition()
+    dispatch(addAction)
+  };
+
+  const onSubClick = () => {
+    const subAction = substraction()
+    dispatch(subAction)
+  };
+  const onMultClick = () => {
+    const multAction = multiplication()
+    dispatch(multAction)
+  }
+  const onDivClick = () => {
+    const divAction = divition()
+    dispatch(divAction)
+  }
+  const onSqrClick = () => {
+    const sqrAction = squareRoot()
+    dispatch(sqrAction)
+  }
+  const onSumClick = () => {
+    const sumAction = summation()
+    dispatch(sumAction)
+  }
+  const onUndoClick = () => {
+    const undoAction = undo()
+    dispatch(undoAction)
+  }
+
+  const onIntroClick = () => {
+    const introAction = intro()
+    dispatch(introAction)
+  }
+
+
 
   return (
     <div className={styles.main}>
@@ -37,17 +85,17 @@ export const Calculator = () => {
         <button className={styles.zeroNumber} onClick={() => onClickNumber(0)}>
           0
         </button>
-        <button onClick={() => onClick()}>.</button>
+        <button onClick={() => onDotClick()}>.</button>
       </div>
       <div className={styles.opKeyContainer}>
-        <button onClick={() => onClick()}>+</button>
-        <button onClick={() => onClick()}>-</button>
-        <button onClick={() => onClick()}>x</button>
-        <button onClick={() => onClick()}>/</button>
-        <button onClick={() => onClick()}>√</button>
-        <button onClick={() => onClick()}>Σ</button>
-        <button onClick={() => onClick()}>Undo</button>
-        <button onClick={() => onClick()}>Intro</button>
+        <button onClick={() => onAddClick()}>+</button>
+        <button onClick={() => onSubClick()}>-</button>
+        <button onClick={() => onMultClick()}>x</button>
+        <button onClick={() => onDivClick()}>/</button>
+        <button onClick={() => onSqrClick()}>√</button>
+        <button onClick={() => onSumClick()}>Σ</button>
+        <button onClick={() => onUndoClick()}>Undo</button>
+        <button onClick={() => onIntroClick()}>Intro</button>
       </div>
       <div className={styles.stack}>{stack.map(renderStackItem)}</div>
     </div>
